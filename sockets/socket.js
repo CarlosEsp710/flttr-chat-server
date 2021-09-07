b14f4a5766a4fd376c1c73a0e7e007c8350fc8f7
Carlos Espejel <carlosespejel71@gamil.com>
2021-09-07 18:39:15 -0500

    Autenticar token

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,15 @@
+const { validateJWT } = require("../helpers/jwt");
 const { io } = require("../index");
 
 // Sockets messages
 io.on("connection", (client) => {
   console.log("Cliente conectado");
+
+  const [validate, uid] = validateJWT(client.handshake.headers["x-token"]);
+
+  if (!validate) {
+    return client.disconnect();
+  }
 
   client.on("disconnect", () => {
     console.log("Cliente desconectado");
