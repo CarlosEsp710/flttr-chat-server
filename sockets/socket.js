117f918c4869fa5307048776c0c4aeaf6c61e018
Carlos Espejel <carlosespejel71@gamil.com>
2021-09-10 14:13:17 -0500

    Cambiar el estado de la conexión de los usuarios

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,17 +1,23 @@
 const { validateJWT } = require("../helpers/jwt");
 const { io } = require("../index");
+const { userConnected, userDisconnected } = require("../controllers/socket");
 
 // Sockets messages
 io.on("connection", (client) => {
   console.log("Cliente conectado");
-  console.log(client.handshake.headers);
   const [validate, uid] = validateJWT(client.handshake.headers["x-token"]);
 
+  // Verify auth
   if (!validate) {
     return client.disconnect();
   }
 
+  // Client connected
+  userConnected(uid);
+
   client.on("disconnect", () => {
     console.log("Cliente desconectado");
+    // Client disconnected
+    userDisconnected(uid);
   });
 });
